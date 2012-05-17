f503bc18e1026f8d6f28cc1604e1f11725123f08
Alex Yu <alex@severalnines.com>
2012-05-17 18:57:24 +0200

    Add mysql root password override

--- roles/cc_agent.rb
+++ roles/cc_agent.rb
@@ -3,3 +3,4 @@ description "ClusterControl Agent"
 run_list ["recipe[cmon::agent_packages]", "recipe[cmon::agent]"]
 #override_attributes({ "mysql" => {"install_dir" => "/usr/local"}})
 #override_attributes({ "mysql" => {"mysql_bin" => "/usr/local/mysql/bin/mysql"}})
+#override_attributes({ "mysql" => {"root_password" => "password"}})
