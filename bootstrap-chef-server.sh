12350919fc05123f9b11858cb66545a11041afa5
Alex Yu <alex@severalnines.com>
2012-05-17 11:41:17 +0200

    Add bootstrap script for chef-server

---
+++ bootstrap-chef-server.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+echo "This script installs Chef on your server (debian/ubuntu)"
+echo "Press return key to continue or CTRL-C to abort"
+read x
+
+echo "deb http://apt.opscode.com/ `lsb_release -cs`-0.10 main" | sudo tee /etc/apt/sources.list.d/opscode.list
+sudo mkdir -p /etc/apt/trusted.gpg.d
+sudo mkdir -p /etc/apt/trusted.gpg.d
+gpg --keyserver keys.gnupg.net --recv-keys 83EF826A
+gpg --export packages@opscode.com | sudo tee /etc/apt/trusted.gpg.d/opscode-keyring.gpg > /dev/null
+sudo apt-get -y update
+sudo apt-get -y install opscode-keyring
+sudo apt-get -y upgrade
+sudo apt-get -y remove chef chef-server
+sudo apt-get autoremove
+sudo apt-get autoclean
+sudo rm -rf /etc/chef
+sudo apt-get -y install chef chef-server
+
+rm -rf .chef
+mkdir -p .chef
+cp /etc/chef/validation .chef/
+cp /etc/chef/webui.pem .chef/
+
+knife configure -i
+
