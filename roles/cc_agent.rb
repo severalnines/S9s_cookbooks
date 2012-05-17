f02269766af8fc4dd49c06dd311c068e4b205de9
Alex Yu <alex@severalnines.com>
2012-05-17 09:46:33 +0200

    Add override attribute example for cc_agent role

--- roles/cc_agent.rb
+++ roles/cc_agent.rb
@@ -1,3 +1,5 @@
 name "cc_agent"
 description "ClusterControl Agent"
 run_list ["recipe[cmon::agent_packages]", "recipe[cmon::agent]"]
+#override_attributes({ "mysql" => {"install_dir" => "/usr/local"}})
+#override_attributes({ "mysql" => {"mysql_bin" => "/usr/local/mysql/bin/mysql"}})
