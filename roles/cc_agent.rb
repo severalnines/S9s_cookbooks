151beb80cee9bb0bfa62e2d268f6399591f159a6
Alex Yu <alex@severalnines.com>
2012-05-24 17:56:27 +0200

    Fix override mysql attribute in cc_agent

--- roles/cc_agent.rb
+++ roles/cc_agent.rb
@@ -1,6 +1,10 @@
 name "cc_agent"
 description "ClusterControl Agent"
 run_list ["recipe[cmon::agent_packages]", "recipe[cmon::agent]"]
-#override_attributes({ "mysql" => {"install_dir" => "/usr/local"}})
-#override_attributes({ "mysql" => {"mysql_bin" => "/usr/local/mysql/bin/mysql"}})
-#override_attributes({ "mysql" => {"root_password" => "password"}})
+#override_attributes({ 
+#  "mysql" => {
+#              "install_dir" => "/usr/local",
+#              "mysql_bin" => "/usr/local/mysql/bin/mysql",
+#              "root_password" => "password"}
+#   }
+#)
